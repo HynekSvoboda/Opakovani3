e2c8976ef00faca8c363413604dbcfd74b98f3a5
HynekSvoboda <97514770+HynekSvoboda@users.noreply.github.com>
2023-09-24 21:02:59 +0200

    Add files via upload

---
+++ Opakovani3/Opakovani3/Form1.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Opakovani3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.CenterToScreen();
+        }
+
+        string Vek(string vstup, out double vek, ref int soucet)
+        {
+            try
+            {
+                string[] pole = vstup.Split(';');
+                string rok = pole[2].Substring(0, 2);
+                soucet += Convert.ToInt32(pole[1]);
+                int mesic = Convert.ToInt32(pole[2].Substring(2, 2));
+                int den = Convert.ToInt32(pole[2].Substring(4, 2));
+                if (int.Parse(rok) > 23)
+                {
+                    rok = "19" + rok;
+                }
+                else rok = "20" + rok;
+                if (mesic > 12) mesic -= 50; ;
+                DateTime narozeni = new DateTime(int.Parse(rok), mesic, den);
+                vek = Math.Round((DateTime.Now - narozeni).TotalDays / 365.25, 1);
+
+                switch (mesic)
+                {
+                    case 1: return "Leden";
+                    case 2: return "Únor";
+                    case 3: return "Březen";
+                    case 4: return "Duben";
+                    case 5: return "Květen";
+                    case 6: return "Červen";
+                    case 7: return "Červenec";
+                    case 8: return "Srpen";
+                    case 9: return "Září";
+                    case 10: return "Říjen";
+                    case 11: return "Listopad";
+                    case 12: return "Prosinec";
+                    default: return "";
+                }
+            }
+            catch
+            {
+                vek = 0;
+                return "";
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            StreamReader cteni;
+            StreamWriter zapis;
+            double vek;
+            bool prvni = false;
+            listBox1.Items.Clear();
+            cteni = new StreamReader("rodna_cis.txt");
+            int soucet = 0;
+            int pocet = 0;
+            string jmeno;
+            List<string> seznam = new List<string>();
+            while(!cteni.EndOfStream)
+            { 
+                string radek = cteni.ReadLine();
+                listBox1.Items.Add(radek);
+                string mesic = Vek(radek, out vek,ref soucet);
+                seznam.Add(radek + ";" + vek);
+                if (radek.Contains(";")) jmeno = radek.Substring(0, radek.IndexOf(";"));
+                else jmeno = "";
+                if (mesic== "Prosinec"&&!prvni)
+                {
+                    MessageBox.Show("první člověk narozený v prosinci: " + jmeno);
+                    prvni = true;
+                }
+                pocet++;
+            }
+            cteni.Close();
+            zapis = new StreamWriter("rodna_cis.txt",false);
+            double prumer = soucet / pocet;
+            int i = 0;
+            foreach(string s in listBox1.Items)
+            {
+                zapis.WriteLine(seznam[i]);
+                i++;
+            }
+            zapis.WriteLine(prumer);
+            zapis.Close();
+            cteni = new StreamReader("rodna_cis.txt");
+            while (!cteni.EndOfStream)
+            {
+                listBox2.Items.Add(cteni.ReadLine());
+            }
+            cteni.Close();
+
+
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.InitialDirectory = Path.GetDirectoryName(Application.ExecutablePath);
+            saveFileDialog.DefaultExt = "txt";
+            string file = "";
+            while (file == "")
+            {
+                if (saveFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    file = saveFileDialog.FileName;
+                }
+            }
+
+            zapis = new StreamWriter(file, false);
+             i = 0;
+            foreach (string s in listBox1.Items)
+            {
+                string[] pole = s.Split(';');
+                if (Convert.ToInt32(pole[1]) < 3)
+                {
+                    seznam[i] +=" "+ Vek(s, out vek,ref soucet);
+                    zapis.WriteLine(seznam[i]);
+                }
+                i++;
+            }
+            zapis.WriteLine(prumer);
+            zapis.Close();
+
+            cteni = new StreamReader(file);
+            while (!cteni.EndOfStream)
+            {
+                string r = cteni.ReadLine();
+                listBox3.Items.Add(r);
+            }
+            cteni.Close();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Příklad 5.\r\nVytvořte metodu Vek, která z rodného čísla vrátí věk a slovně měsíc narození.\r\nJe dán textový soubor rodna_cis.txt. Na každém řádku je jméno, středník, známka,\r\nstředník, rodné číslo. Soubor vypište do ListBoxu.\r\nSpočítejte průměr známek všech lidí, který zapište na konec textového souboru\r\nVypište prvního člověka narozeného v prosinci, nezapomeňte, že muž má druhé dvojčíslí\r\nměsíc 12 a žena 62. Využijte metodu Vek\r\nSoubor rodna_cis.txt, opravte tak, že na konec každého řádku přidejte středník a za něj\r\nzapište věk. Využijte metodu Vek. Soubor vypište do dalšího ListBoxu.\r\nVytvořte nový textový soubor, který bude obsahovat jméno člověka, středník, věk,\r\nstředník, slovně měsíc jen těch lidí, kteří mají známku lepší než 3. Název nového textového\r\nsouboru vyberte pomocí SaveFileDialogu. Soubor vypište do dalšího ListBoxu.");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Form f2 = new Form2();
+            f2.ShowDialog();
+        }
+    }
+}
---
+++ Opakovani3/Opakovani3/Form2.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Opakovani3
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+            this.CenterToScreen();
+            button2.Visible=false;
+            button3.Visible=false;
+            button4.Visible=false;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            button3.Visible = false;
+            button4.Visible = false;
+            File.Delete("Cisla.dat");
+            OpenFileDialog op = new OpenFileDialog();
+            op.DefaultExt = "txt";
+            op.InitialDirectory= Path.GetDirectoryName(Application.ExecutablePath);
+            string file="";
+            if (op.ShowDialog() == DialogResult.OK)
+            {
+                file = op.FileName;
+                button2.Visible = true;
+                StreamReader cteni = new StreamReader(file);
+                FileStream tok = new FileStream("Cisla.dat", FileMode.OpenOrCreate, FileAccess.Write);
+                BinaryWriter zapis = new BinaryWriter(tok);
+                zapis.BaseStream.Position = 0;
+                while (!cteni.EndOfStream)
+                {
+                    string line = cteni.ReadLine();
+                    string[] pole = line.Split(';');
+                    double max = 0;
+                    for (int i = 0; i < pole.Count(); i++)
+                    {
+                        if (max < pole[i].Length)
+                        {
+                            max = pole[i].Length;
+                        }
+                    }
+                    max /= 10;
+                    zapis.Write(max);
+                    max = 0;
+                }
+                cteni.Close();
+
+                tok.Close();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            button3.Visible=true;
+            listBox1.Items.Clear();
+            FileStream tok = new FileStream("Cisla.dat", FileMode.OpenOrCreate, FileAccess.Read);
+            BinaryReader bincteni = new BinaryReader(tok);
+            bincteni.BaseStream.Position = 0;
+            while (bincteni.BaseStream.Position < bincteni.BaseStream.Length)
+            {
+                listBox1.Items.Add(bincteni.ReadDouble());
+            }
+            tok.Close();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            button4.Visible=true;
+            FileStream tok = new FileStream("Cisla.dat", FileMode.OpenOrCreate, FileAccess.Write);
+            BinaryWriter zapis = new BinaryWriter(tok);
+            zapis.BaseStream.Position = 0;
+            foreach(double i in listBox1.Items)
+            {
+                double cislo = i;
+                if (cislo < 1) cislo *= 10;
+                zapis.Write(cislo);
+            }
+            tok.Close();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            FileStream tok = new FileStream("Cisla.dat", FileMode.OpenOrCreate, FileAccess.ReadWrite);
+            BinaryReader bincteni = new BinaryReader(tok);
+            BinaryWriter zapis = new BinaryWriter(tok);
+            bincteni.BaseStream.Position = 0;
+            double pocet=0;
+            double soucet = 0;
+            while (bincteni.BaseStream.Position < bincteni.BaseStream.Length)
+            {
+                double cislo= bincteni.ReadDouble();
+                if (cislo > 2)
+                {
+                    pocet++;
+                    soucet += cislo;
+                }
+            }
+            double prumer = soucet / pocet;
+            zapis.BaseStream.Position = zapis.BaseStream.Length;
+            zapis.Write(prumer);
+            MessageBox.Show("Průměr: " + prumer);
+            tok.Close();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Příklad 6.\r\nButton1\r\nV textovém souboru vybraném pomocí OpenFileDialogu je na každém řádku zapsáno více\r\nslov oddělených středníkem. Tento soubor zobrazte.\r\nV každém řádku souboru najděte nejdelší slovo a délky těchto nejdelších slov podělte deseti\r\na výsledné reálné číslo zapisujte do nově vytvořeného datového souboru reálných čísel\r\nCisla.dat.\r\nPříklad textového souboru:\r\nPes;kočka;kuřátko\r\nrůže;sedmikráska,pýr\r\npavouk;brouk\r\nK němu vytvořený soubor cisla.dat bude obsahovat reálná čísla:\r\n0,7\n1,5\r\n0,6\r\nButton2\r\nZobrazte soubor Cisla.dat\r\nButton3\r\nV souboru Cisla.dat opravte všechna čísla menší než 1 na desetinásobek.\r\nOpravený soubor\r\n7\n1,5\r\n6\r\nButton4\r\nV souboru Cisla.dat spočtěte aritmetický průměr ze všech čísel větších než 2 a tento\r\naritmetický průměr zapište na konec souboru.\r\nOpravený soubor\r\n7\r\n1,1\r\n6\r\n6,5");
+        }
+    }
+}
